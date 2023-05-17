ef71dc1f95dd64c523db931c29e6bfd178ed7fb8
Tomah <959104@cognizant.com>
2023-05-17 13:04:58 -0500

    initial commit

---
+++ src/App.java
@@ -0,0 +1,42 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        //User Input
+        Scanner sc = new Scanner(System.in);
+        //Random
+        Random random = new Random();
+        System.out.println("Welcome to Rock Paper Scissors");
+        System.out.println("Rock = 1, Scissors = 2, Paper = 3");
+
+        System.out.println("Player 1's Turn");
+        //User Input
+        int p1 = sc.nextInt();
+        System.out.println("Player 2's Turn");
+        //Parameters of Random
+        int p2 = random.nextInt(1,4);
+
+
+        System.out.println("Player 1 selected: " + p1);
+        System.out.println("Player 2 selected: " + p2);
+
+
+        //if conditionals for Rock, Paper, Scissors
+        if(p1 == 1 && p2 == 2){
+            System.out.println("Player 1 Wins");
+        } else if (p1 == 2 && p2 == 3){
+            System.out.println("Player 1 Wins");
+        } else if (p1 == 3 && p2 == 1){
+            System.out.println("Player 1 Wins");
+        } else if (p1 == 2 && p2 == 1){
+            System.out.println("Player 2 Wins");
+        } else if (p1 == 3 && p2 == 2){
+            System.out.println("Player 2 Wins");
+        } else if (p1 == 1 && p2 == 3){
+            System.out.println("Player 2 Wins");
+        } else {
+            System.out.println("Tie");
+        }
+    }
+}
